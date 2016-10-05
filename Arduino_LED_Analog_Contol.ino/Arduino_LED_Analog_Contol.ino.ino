255fc5377d48df20e4e63168e1f5ca5845752a43
jungseungho1 <Jung@SEUNGs-MBP.wireless.newschool.edu>
2016-10-04 21:08:25 -0400

    photoseonsor

--- Arduino_LED_Analog_Contol.ino/Arduino_LED_Analog_Contol.ino.ino
+++ Arduino_LED_Analog_Contol.ino/Arduino_LED_Analog_Contol.ino.ino
@@ -1,23 +1,23 @@
 /*
   Analog input, analog output, serial output
 
- Reads an analog input pin, maps the result to a range from 0 to 255
- and uses the result to set the pulsewidth modulation (PWM) of an output pin.
- Also prints the results to the serial monitor.
+  Reads an analog input pin, maps the result to a range from 0 to 255
+  and uses the result to set the pulsewidth modulation (PWM) of an output pin.
+  Also prints the results to the serial monitor.
 
- The circuit:
- * potentiometer connected to analog pin 0.
+  The circuit:
+   potentiometer connected to analog pin 0.
    Center pin of the potentiometer goes to the analog pin.
    side pins of the potentiometer go to +5V and ground
- * LED connected from digital pin 9 to ground
+   LED connected from digital pin 9 to ground
 
- created 29 Dec. 2008
- modified 9 Apr 2012
- by Tom Igoe
+  created 29 Dec. 2008
+  modified 9 Apr 2012
+  by Tom Igoe
 
- This example code is in the public domain.
+  This example code is in the public domain.
 
- */
+*/
 
 // These constants won't change.  They're used to give names
 // to the pins used:
@@ -36,7 +36,12 @@ void loop() {
   // read the analog in value:
   sensorValue = analogRead(analogInPin);
   // map it to the range of the analog out:
-  outputValue = map(sensorValue, 0, 1023, 0, 255);
+  outputValue = map(sensorValue, 350, 850, 0, 255);
+
+  if (outputValue <= 0) {
+    outputValue = 0;
+  }
+
   // change the analog out value:
   analogWrite(analogOutPin, outputValue);
 
