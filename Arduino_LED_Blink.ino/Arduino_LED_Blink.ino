f4beda7e9aaa91c6c698291d73bdf6bff8642555
jungseungho1 <Jung@SEUNGs-MBP.wireless.newschool.edu>
2016-10-04 20:38:10 -0400

    test3

---
+++ Arduino_LED_Blink.ino/Arduino_LED_Blink.ino
@@ -0,0 +1,21 @@
+int led = 13;
+int buttonPin = 2;
+int buttonState = 0;
+
+void setup(){
+  pinMode(led, OUTPUT);
+  pinMode(buttonPin, INPUT);
+}
+
+void loop(){
+  buttonState = digitalRead(buttonPin);
+
+  if(buttonState == HIGH){
+    digitalWrite(led, HIGH);
+//    delay (3000);
+  } else {
+    digitalWrite(led, LOW);
+//    delay(10);
+  }
+}
+
---
+++ Arduino_LED_Blink.ino/README.md
@@ -0,0 +1,11 @@
+# Seung ho Jung
+## Parsons MFADT
+### CCLab2016
+
+Physical Computing:
+
+* Arduino
+
+
+[video demo](https://youtu.be/ntPNY4GQcd0)
+
